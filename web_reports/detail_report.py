6d31fa0c4766379a6caa56a77e0bf2c2f1ee9ce4
Ayumu-s <ayumuka34@gmail.com>
2026-04-16 17:10:21 +0900

    更新

--- web_reports/detail_report.py
+++ web_reports/detail_report.py
@@ -1,3 +1,1 @@
-﻿from web_reports.detail_report_helpers import *
-from web_reports.detail_report_sections import *
-from web_reports.detail_report_main import *
+from reports.excel.exports.detail_report import *
